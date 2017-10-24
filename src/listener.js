02c656eba1900ca74a31ffb467cefa9ab30f2442
Tamas Nepusz <ntamas@gmail.com>
2017-10-24 16:54:25 +0200

    migrating to prop-types from React.PropTypes

--- src/listener.js
+++ src/listener.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { NAME, get } from './socket';
 
 class Listener extends Component {
